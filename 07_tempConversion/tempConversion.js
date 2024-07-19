0f65c3327b14eed75720e850623bc60ee0eaa70b
mysia3 <mingyang095@gmail.com>
2024-07-19 16:38:43 +0800

    07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(a) {
+  a = a -32
+  a = a * 5 / 9
+  return Math.round(a * 10) /10
+
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(a) {
+  a = a * 9/5
+  a = a + 32
+  return Math.round(a * 10) /10
+  
 };
 
 // Do not edit below this line
