a54e804dbe6defb7e38c5160f78ea99feb1c0f5f
mysia3 <mingyang095@gmail.com>
2024-07-19 15:19:48 +0800

    worked

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...x) {
+    for (const arg of x) {
+      for (let i = 0; i < arr.length; i++) {
+          if (arr[i] == arg ) {
+            if (typeof arr[i] == typeof arg) {
+            arr.splice(i,1);
+            i = i- 1;}
+          }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
