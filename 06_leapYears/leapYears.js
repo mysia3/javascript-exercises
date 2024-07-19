90e6dc7e2abc12eb96a0a2c38db2f25e05e92542
mysia3 <mingyang095@gmail.com>
2024-07-19 16:23:47 +0800

    finish 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(a) {
+    if (a % 4 == 0) {
+        if (a % 400 == 0) {
+            return true
+        }
+        if (a % 100 == 0) {
+            return false
+        }
+        return true
+    }
+    return false
 };
 
 // Do not edit below this line
